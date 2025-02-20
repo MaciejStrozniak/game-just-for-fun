c3d9b1035fd511505914ef89d7b2145c3179b5d6
msCDV <macgitcdv@gmail.com>
2025-02-20 21:47:56 +0100

    Updated control over damaging the enemies and made a preperation for picups system

--- Assets/Scripts/CollisionWithPlayer.cs
+++ Assets/Scripts/CollisionWithPlayer.cs
@@ -1,31 +1,46 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class CollisionWithPlayer : MonoBehaviour
 {
     int countdownToExtinction = 0;
-    bool destroyActive = false;
+    private Player player;
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.O) && destroyActive == false) {
-            destroyActive = !destroyActive;
-            Debug.Log(destroyActive);
-        }
-        else if(Input.GetKeyDown(KeyCode.O) && destroyActive == true)
-            destroyActive = !destroyActive;
+        if(Input.GetKeyDown(KeyCode.O)) 
+        {
+            if(player.isCausingDamage == false)
+            {
+                player.isCausingDamage = !player.isCausingDamage;
+                Debug.Log(player.isCausingDamage);
+            }
+            else if(player.isCausingDamage == true)
+            {
+                player.isCausingDamage = !player.isCausingDamage;
+                Debug.Log(player.isCausingDamage);
+            }
+        }       
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if(collision.gameObject.tag == "Player" && player.isCausingDamage == false)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = UnityEngine.Random.ColorHSV();       
+        }        
+        else if(collision.gameObject.tag == "Player" && player.isCausingDamage == true)
         {
             gameObject.GetComponent<SpriteRenderer>().color = UnityEngine.Random.ColorHSV();
             countdownToExtinction ++;
-            if(countdownToExtinction == 3 && destroyActive == true)
+            if(countdownToExtinction >= 3 && player.isCausingDamage)
                 Destroy(gameObject);
         }
     }
