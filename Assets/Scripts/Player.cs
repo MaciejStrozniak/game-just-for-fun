dd62957154e48bb0dc568e14daf78afd2657bf9d
msCDV <macgitcdv@gmail.com>
2025-02-21 15:37:34 +0100

    First try to work with json files- work in progress

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,27 @@ public class Player : MonoBehaviour
     public bool isCausingDamage = false;
     public int numberOfPickups = 0;
 
+    public TextAsset saveFile;
+
+    [System.Serializable]
+    public class SaveData
+    {
+        public string pickUps;
+    }
+
+    [System.Serializable]
+    public class DataList
+    {
+        public SaveData[] datas;
+    }
+
+    public SaveData[] datasToStore;
+
+    void Start()
+    {
+        // DataList dataList = JsonUtility.FromJson<DataList>();
+    }
+
 }
 
 
