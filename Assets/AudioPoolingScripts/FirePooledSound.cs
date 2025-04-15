6f117935df0264c994c4811cfd2e69731420ef5a
msCDV <macgitcdv@gmail.com>
2025-04-15 18:23:33 +0200

    Added audio pool system to the game. And it works! :D Smok Win 11

--- Assets/AudioPoolingScripts/FirePooledSound.cs
+++ Assets/AudioPoolingScripts/FirePooledSound.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class FirePooledSound : MonoBehaviour
 {
+    [SerializeField] private AudioClip soundToFire;
+    [SerializeField] private float volume = 1f;
+    [SerializeField] private float playInterval = 0.3f;
 
-    GameObject gameObjectToFire = gameObject.GetComponent<PooledAudioSource>();
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float nextPlayTime = 0;
+    private bool isPlaying = false;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            isPlaying = !isPlaying;
+        }
+
+        if(isPlaying && Time.time >= nextPlayTime)
+        {
+            AudioPool.Instance.PlayOneShot(soundToFire, transform.position, volume);
+            nextPlayTime = Time.time + playInterval;
+        }
     }
 }
