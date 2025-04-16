8161b4bd73e0a055f6d5b77b45f2380ee2ee1b5a
msCDV <macgitcdv@gmail.com>
2025-04-16 23:44:25 +0200

    Practicing connecting differents scripts together. Smok Win 11

--- Assets/Scripts/SharingScripts/Methods.cs
+++ Assets/Scripts/SharingScripts/Methods.cs
@@ -1,8 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Methods : MonoBehaviour
 {
+    public AudioClip setAudioClip(AudioClip getAudioClip)
+    {
+        AudioClip setAudioClip = getAudioClip;
+
+        return setAudioClip;
+    }
+
+    public void startAudioSource(AudioSource audioSource, AudioClip audioClip)
+    {
+        audioSource.clip = audioClip;
+        audioSource.Play();
+    }
+
+    public AudioSource setNewVolume(AudioSource audioSourceToSet, float volume)
+    {
+        audioSourceToSet.volume = volume;
+
+        return audioSourceToSet;
+    }
 
 }
--- Assets/Scripts/SharingScripts/MethodsReader.cs
+++ Assets/Scripts/SharingScripts/MethodsReader.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MethodsReader : MonoBehaviour
 {
+    public Methods methods;
+
+    [SerializeField] private AudioClip audioClipFromInspector;
+    [SerializeField] private AudioSource thisAudioSource;
+    [SerializeField] private float volume = 0;
+
+    private float waitUntilTheTimeIsRigth = 5f;
+    private bool debugAudioMute = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        thisAudioSource = gameObject.AddComponent<AudioSource>();
+        methods.startAudioSource(thisAudioSource, methods.setAudioClip(audioClipFromInspector));    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        waitUntilTheTimeIsRigth -= Time.deltaTime;
+
+        if(waitUntilTheTimeIsRigth >= 0)
+            Debug.Log(waitUntilTheTimeIsRigth);
+
+
+        else if(waitUntilTheTimeIsRigth <= 0 && debugAudioMute)
+        {
+            methods.setNewVolume(thisAudioSource, volume);
+            Debug.Log("Music mute!");
+            debugAudioMute = !debugAudioMute;
+        }
     }
 }
